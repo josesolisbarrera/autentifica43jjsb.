0e79071218550f4cf2690645a89373102d4a361f
josesolisbarrera <33911914+josesolisbarrera@users.noreply.github.com>
2021-03-22 20:50:07 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,10 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyAY9CYAe06C1AhAXJdPHlncoYWrsXA3evk",
+   apiKey: "AIzaSyAY9CYAe06C1AhAXJdPHlncoYWrsXA3evk",
     authDomain: "autentifica43jjsb-5ff41.firebaseapp.com",
     projectId: "autentifica43jjsb-5ff41",
     storageBucket: "autentifica43jjsb-5ff41.appspot.com",
     messagingSenderId: "149647562040",
-    appId: "1:149647562040:web:b37556761a80c56f3a9dcd"
+    appId: "1:149647562040:web:19c9c1ad937996b63a9dcd"
     });
